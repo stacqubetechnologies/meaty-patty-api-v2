0b49c3a8bcb18dc451fbdd80ffde62d687fcf8c9
stacqubetechnologies <backtickhost@gmail.com>
2024-11-29 00:15:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# meaty-patty-api
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const menuRoutes = require('./routes/menuRoute');
+const paymentRoutes = require('./routes/paymentRoutes');
+const smsRoutes = require('./routes/smsRoutes');
+const authRoute = require('./routes/authRoute');
+const orderRoute = require('./routes/orderRoute');
+
+app.use(express.json()); // Parse incoming requests
+app.use(cors());
+app.use('/api/menu', menuRoutes);
+app.use('/api/payments', paymentRoutes);
+app.use('/api/sms', smsRoutes);
+app.use('/api/user', authRoute);
+app.use('/api/orders', orderRoute);
+module.exports = app;
