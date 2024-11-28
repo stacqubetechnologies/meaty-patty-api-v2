2ccb0b7c6eae4b5112c3496a14c5f58c3c7eea85
Staqube <backtickhost@gmail.com>
2024-11-29 00:20:38 +0530

    New Update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,5 +6,8 @@ router.post('/login', userController.loginUser);
 router.post('/register', userController.registerUser);
 router.post('/add-address', userController.addAddressController);
 router.post('/get-addressbyuser', userController.getAddressListController);
+router.get('/', (req, res) => {
+    res.send('Order API is working!');
+  });
 
 module.exports = router;
