b47ec71ead997958b1dd689cd7c1ce428c820271
Staqube <backtickhost@gmail.com>
2024-12-25 22:30:53 +0530

    Api Changes 25 Dec V2

--- config/appwrite.js
+++ config/appwrite.js
@@ -1,12 +1,11 @@
-require('dotenv').config(); 
+require('dotenv').config()
 
-const { Client } = require('appwrite');
+const { Client } = require('appwrite')
 
-
-const client = new Client();
+const client = new Client()
 
 client
-    .setEndpoint(process.env.APPWRITE_ENDPOINT) 
-    .setProject(process.env.APPWRITE_PROJECT_ID); 
+  .setEndpoint(process.env.APPWRITE_ENDPOINT)
+  .setProject(process.env.APPWRITE_PROJECT_ID)
 
-module.exports = { client, apiKey: process.env.APPWRITE_API_KEY };
+module.exports = { client, apiKey: process.env.APPWRITE_API_KEY }
