af8d17c9b2227d8d6ce24783b44fbec2e3f61c27
Staqube <backtickhost@gmail.com>
2024-12-28 12:36:28 +0000

    stripe key

--- config/stripe.js
+++ config/stripe.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_PROD);
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 module.exports = stripe;
 
